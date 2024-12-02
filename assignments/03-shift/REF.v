9f92ac7ef34e6c67a1ed9d7a0c3326d5557e4c50
Walthzer <s.v.feitsma@gmail.com>
2024-12-02 23:09:37 +0100

    update

--- assignments/03-shift/REF.v
+++ assignments/03-shift/REF.v
@@ -11,43 +11,45 @@ module	REF (
 		input reset,
 		input enable,
 		input serial_in,
-		output serial_out,
-		output [7:0] data_out,
+		output serial_out
 	);
 	reg [7:0] register;
 
-
 	always @(posedge clk) begin
+		register <= register;
+
 		if (reset) begin
 			register <= 8'b0;
 		end else if (enable) begin
-			register <= {register[7:1], serial_in};
+			register <= register << 1;
+			register[0] <= serial_in;
 		end
 	end
 
 	assign serial_out = register[7];
 
 `ifdef FORMAL
-	reg f_past_valid;
+	reg f_is_reset;
 
 	initial begin
-		f_past_valid = 0;
+		f_is_reset = 0;
 	end
 
 	always @(posedge clk) begin
 		//track if rest into valid state
 		if(reset)
-			f_past_valid <= 1;
+			f_is_reset <= 1;
 
 		//Data shifted
-		if(f_past_valid && $past(enable) && !$past(reset)) begin
+		if(f_is_reset && $past(enable) && !$past(reset)) begin
 			assert(register[0] == $past(serial_in));
-			assert(serial_out == $past(register[7]));
 		end
 	end
 
 	always @(*) begin
-		assume(serial_in != 1'b0);
+		if(f_is_reset) begin
+			cover(register[7] != 1'b0);
+		end
 	end
 `endif
 
