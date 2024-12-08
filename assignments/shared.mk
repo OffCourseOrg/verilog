101c5b73f04ae95f10de907ccae83ce033b4f3ec
Walthzer <s.v.feitsma@gmail.com>
2024-12-08 18:59:08 +0100

    update shared

--- assignments/shared.mk
+++ assignments/shared.mk
@@ -1,7 +1,9 @@
+CURRENT_DIR = $(shell pwd)
 TOOLS-DIR=../../tools/
 OSS-CAD-PATH=/opt/oss-cad-suite
 OSS-CAD-BIN=$(OSS-CAD-PATH)/bin
 DIR_NAME=sby_tmp
+
 
 FSM2GRAPH=python $(TOOLS-DIR)/fsm2graph
 
@@ -27,6 +29,12 @@ fsm: env
 fsm_int: env
 	$(OSS-CAD-BIN)/yosys -p 'read_verilog -D GEN_FSM REF.v; proc; opt -nodffe -nosdff; autoname; fsm -nomap -norecode; write_table tmp/netlist.txt' | tee tmp/yosys.txt | $(FSM2GRAPH) --info --netlist tmp/netlist.txt
 	xdot tmp/fsm.gv
+
+diagram: env
+	$(OSS-CAD-BIN)/yosys -p "prep -top REF; write_json tmp/netlist.json" REF.v
+	yarn netlistsvg-offcourse $(CURRENT_DIR)/tmp/netlist.json -o $(CURRENT_DIR)/tmp/diagram.svg
+	xdg-open tmp/diagram.svg
+
 
 env:
 	source $(OSS-CAD-PATH)/environment
