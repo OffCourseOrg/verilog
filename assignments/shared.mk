dd971d9c8d3c0d8d5b12502c2555d2d821f9ef2e
Walthzer <s.v.feitsma@gmail.com>
2024-12-16 22:49:02 +0100

    update make

--- assignments/shared.mk
+++ assignments/shared.mk
@@ -22,31 +22,33 @@ cvr: $(SOURCE) env
 prv: $(SOURCE) env
 	$(OSS-CAD-BIN)/sby -f $< $@ --prefix tmp
 
-fsm: env sby_tasks
-	rm tmp/fsm.gv
+fsm: env clean sby_tasks
 	$(OSS-CAD-BIN)/yosys -p 'read_verilog -D GEN_FSM REF.v; proc; opt -nodffe -nosdff; autoname; fsm -nomap -norecode; write_table tmp/netlist.txt' | tee tmp/yosys.txt | $(FSM2GRAPH) --info --verilog REF.v --netlist tmp/netlist.txt
+	dot -Tsvg tmp/fsm.gv -o fsm.svg
 	xdot tmp/fsm.gv
 
-fsm_int: env
+fsm_int: env clean sby_tasks
 	$(OSS-CAD-BIN)/yosys -p 'read_verilog -D GEN_FSM REF.v; proc; opt -nodffe -nosdff; autoname; fsm -nomap -norecode; write_table tmp/netlist.txt' | tee tmp/yosys.txt | $(FSM2GRAPH) --info --netlist tmp/netlist.txt
+	dot -Tsvg tmp/fsm.gv -o fsm.svg
 	xdot tmp/fsm.gv
 
 load_for_tools:
-	rm $(TOOLS-DIR)/tmp/*
+	-rm $(TOOLS-DIR)/tmp/*
 	cp REF.v $(TOOLS-DIR)/tmp/REF.v
 	cp tmp/*.txt $(TOOLS-DIR)/tmp/
 
 diagram: env sby_tasks
 	$(OSS-CAD-BIN)/yosys -p "prep -top REF; write_json tmp/netlist.json" REF.v
-	yarn netlistsvg-offcourse $(CURRENT_DIR)/tmp/netlist.json -o $(CURRENT_DIR)/tmp/diagram.svg
-	xdg-open tmp/diagram.svg
+	yarn netlistsvg-offcourse $(CURRENT_DIR)/tmp/netlist.json -o $(CURRENT_DIR)/diagram.svg
+	xdg-open diagram.svg
 
 
 env:
 	source $(OSS-CAD-PATH)/environment
 
 clean:
-	rm -rf tmp*
+	-rm -rf tmp*
+	-rm *.svg
 
 trace: env
 	$(OSS-CAD-BIN)/gtkwave tmp_prv/engine_0/*.vcd
